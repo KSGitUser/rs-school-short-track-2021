b6a0c926dbb27b3617bdcfab0ee87756f5b27dd0
Sergey Krikun <sergey.a.krikun@gmail.com>
2021-04-07 03:08:49 +0300

    feat: 10-find-index

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,24 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let foundIndex;
+  let minIndex = 0;
+  let maxIndex = array.length - 1;
+  while (maxIndex + 1 > minIndex) {
+    const mid = minIndex + Math.floor((maxIndex - minIndex) / 2);
+    if (array[mid] < value) {
+      minIndex = mid + 1;
+    }
+    if (array[mid] > value) {
+      maxIndex = mid - 1;
+    }
+    if (array[mid] === value) {
+      foundIndex = mid;
+      break;
+    }
+  }
+  return foundIndex;
 }
 
 module.exports = findIndex;
